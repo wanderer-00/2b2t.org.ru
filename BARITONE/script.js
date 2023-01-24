3d163ecbbe23eaa5cd4839dc7e6583b2425d2c46
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2023-01-24 13:49:21 +0300

    1

--- BARITONE/script.js
+++ BARITONE/script.js
@@ -6,11 +6,28 @@ for(i=0; i<array.length; i++){
     box.className = "box";
     cluster.append(box);
     
-    let name = document.createElement('h2');
-    name.innerHTML = array[i].name;
+    let name = document.createElement('label');
+    name.className = "name";
+    name.innerHTML = '#' + array[i].name + ' ';
     box.append(name)
+    
+    let arg_box = document.createElement('select');
+    box.append(arg_box)
+    
+    for(n=0; n<array[i].arg.length; n++){
+        let arg = document.createElement('option');
+        arg.innerHTML = array[i].arg[n];
+        arg_box.append(arg)
+    }
     
     let note = document.createElement('p');
     note.innerHTML = array[i].note;
     box.append(note)
 }
+
+//                <label for="">производитель:</label>
+//                <select name="" id="">
+//                    <option value="">---</option>
+//                    <option value="">AMD</option>
+//                    <option value="">Intel</option>
+//                </select>
